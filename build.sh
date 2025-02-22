116523cd8836ae30691d115c93caa71e2273afb3
Naastepad <martin@naastepad.com>
2025-02-22 22:21:32 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -eux
 
-# Installeer alleen de essentiële dependencies
-apt-get update && apt-get install -y \
-    libcairo2 \
-    libcairo2-dev \
-    libfreetype6-dev \
-    libffi-dev \
-    python3-cffi
-
-# Installeer Python packages
-pip install --no-cache-dir cairocffi pycairo freetype-py Pillow
+# Installeer Cairo en afhankelijkheden
+pip install --no-cache-dir cairocffi pycairo Pillow imageio
