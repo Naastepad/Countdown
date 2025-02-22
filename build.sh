7b06134a06488e72b0b220262f5678c27fbef563
Naastepad <martin@naastepad.com>
2025-02-22 17:16:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 
-# Installeer Cairo en afhankelijkheden
+# Installeer Cairo en FreeType voor HarfBuzz
 apt-get update && apt-get install -y \
     libcairo2 \
     libcairo2-dev \
+    libharfbuzz-dev \
+    libfreetype6-dev \
     libffi-dev \
     python3-cffi
 
-# Installeer Python-pakketten
-pip install --no-cache-dir cairocffi pycairo
+# Installeer Python packages
+pip install --no-cache-dir cairocffi pycairo harfbuzz freetype-py Pillow
