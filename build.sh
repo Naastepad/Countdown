812c2f5d0fea20cefa2db3f028d31da42071ca20
Naastepad <martin@naastepad.com>
2025-02-22 17:03:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-# Installeer Cairo en Pango
+# Installeer alleen Cairo en afhankelijkheden
 apt-get update && apt-get install -y \
     libcairo2 \
     libcairo2-dev \
-    libpango1.0-dev \
-    libpangocairo-1.0-0 \
     libffi-dev \
-    python3-cffi \
-    python3-gi \
-    gir1.2-pango-1.0 \
-    gir1.2-gtk-3.0
+    python3-cffi
 
 # Installeer Python dependencies
-pip install --no-cache-dir cairocffi pycairo pygobject
+pip install --no-cache-dir cairocffi pycairo
